1633e369ffe424510f9631ba898e0f59534bd590
Phoenix UK LTD <info@iqiniso.com>
2018-05-20 19:15:54 +0900

    Initial Files for Japanese
    these are the files for the Japanese text.

---
+++ jp/login_jp.php
@@ -0,0 +1,28 @@
+<?php
+
+// app/messages/en.php
+$messages = [
+    "greeting"      => "greeting",
+    "login"     => "ログイン",
+	"header" => "ログインしてください",
+    "moreinfo" => "詳細はこちら",
+	"subtext" => "エイゴモンスターは先生のディジタル味噌汁！",
+	"register" => "登録",
+    "logintext"    => "あなたのエイゴモンスターのアカウントにログイン",
+	"logintagline"     => "教師による教師のためのレッスンプランナー。より少ない計画とより多くの教授",
+	"password"     => "あなたのエイゴモンスターのアカウントにログイン",
+	"mobemail"     => "電子メールアドレス",
+	"loginbutton"     => "ログイン",
+	"required"     => "必須フィールド",
+	"forgot"     => "パスワードを忘れました?",
+	"terms"     => "規約と条件",
+	"privacy"     => "個人情報保護方針",
+	"logging_in"     => "モンスターを探しています。。。",
+	"invalid_login"     => "無効な資格情報",
+	"warn"     => "入力された情報を確認してください",
+	"ph_password"     => "パスワード",
+	"ph_username"     => "ユーザー名",
+	
+];
+
+
---
+++ jp/register_jp.php
@@ -0,0 +1,40 @@
+<?php
+
+// app/messages/en.php
+$messages = [
+	"header" => "簡単にと安全に自分のエイゴモンスターアカウントを作成する.",
+    "moreinfo" => "詳しいはこちら。。。",
+	"signin" => "サインイン",
+	"subtext" => "エイゴモンスターは先生のディジタル味噌汁！",
+    "register_text"    => "登録",
+	"logintagline"     => "アカウントを作成してください",
+	"password"     => "あなたのエイゴモンスターのアカウントにログイン",
+	"mobemail"     => "電子メールアドレス",
+	"loginbutton"     => "ログイン",
+	"required"     => "必須フィールド",
+	"forgot"     => "パスワードを忘れました?",
+	"terms"     => "規約と条件",
+	"privacy"     => "個人情報保護方針",
+	"logging_in"     => "モンスターを探しています。。。",
+	"invalid_login"     => "無効な資格情報",
+	"warn"     => "入力された情報を確認してください。",
+	"ph_password"     => "パスワード",
+	"ph_username"     => "ユーザー名",
+	"country" => "自国",
+	"of"     => "の",
+	"step" => "ステップ",
+	"valid_req" => "メールの確認が必要です",
+	"valid_req_msg" =>"あなたの電子メールにピンコードが送信されました",
+	"reg_complete" => "登録完了です。",
+	"reg_complete_msg" =>"これから頑張ろ！",
+	"valid_err" => "エラー",
+	"valid_err_msg" => "入力された情報は確認できません。",
+	"next_step" => "次へ",
+	"complete_reg" => "登録を完了する",
+	
+	
+	
+	
+];
+
+
